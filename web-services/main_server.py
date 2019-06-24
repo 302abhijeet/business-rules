f993e085a27fa1b68d23d25a501bc263bd5e0f05
Vij <Amit.Vij@viasat.com>
2019-06-24 18:06:55 +0530

    Improved error handling in the server

--- web-services/main_server.py
+++ web-services/main_server.py
@@ -1,6 +1,6 @@
 import business_rules.API as API
+import json
 from filehandler import *
-from werkzeug.exceptions import BadRequest
 from utils import *
 from flask import Flask,request,jsonify,Response
 
@@ -16,11 +16,19 @@ def runrule():
     if request.method == 'POST':
         data_given = request.args.to_dict()
         if 'rule' not in data_given:
-                return BadRequest('please specify a rule')
+                return Response(
+                        response=json.dumps({'Error':'Please specify a rule'}),
+                        mimetype='application/json',
+                        status=400
+                )
         rulename = data_given['rule']
         flag = check_valid_rule(rulename)
         if not flag:
-            return BadRequest('Rule does not exist')
+            return  Response(
+                        response=json.dumps({'Error':'Rule doesnot exist'}),
+                        mimetype='application/json',
+                        status=400
+                )
         del data_given['rule']
         check_valid_data(data_given)
         #check if any data is in body or not
@@ -29,7 +37,11 @@ def runrule():
         #API function to run the rules
         #this function returns a list of messages
         data = API._run_API(run_rule=rulename,case=None,parameter_variables=data_given,parameter_dataSource=parameter_data)
-        return jsonify({'run_msg':'run successful','data':data})
+        return  Response(
+                        response=json.dumps({'run_msg':'Run successful','data':data}),
+                        mimetype='application/json',
+                        status=200
+                )
        
             
 #Server route to run use cases
@@ -39,11 +51,19 @@ def runusecase():
         #get the optional variable values as arguments
         data_given = request.args.to_dict()
         if 'use_case' not in data_given:
-                return BadRequest('Error! please specify a use case')
+               return Response(
+                        response=json.dumps({'Error':'Please specify a Use Case'}),
+                        mimetype='application/json',
+                        status=400
+                )
         ucname = data_given['use_case']
         flag = check_valid_usecase(ucname)
         if not flag:
-            return BadRequest('Use case does not exist')
+            return Response(
+                        response=json.dumps({'Error':'Use case doesnot exist'}),
+                        mimetype='application/json',
+                        status=400
+                )
 
 
         del data_given['use_case']
@@ -53,7 +73,11 @@ def runusecase():
         
         #this function returns a list of messages
         data = API._run_API(run_rule=None,case=ucname,parameter_variables=data_given,parameter_dataSource=parameter_data)
-        return jsonify({'run_msg':'run successful','data':data})
+        return Response(
+                        response=json.dumps({'run_msg':'run successful','data':data}),
+                        mimetype='application/json',
+                        status=200
+                )
 
 
 #Server route to add data
@@ -63,7 +87,11 @@ def addrule(ty):
         
         filepath = updateFilePath(dirpath,ty) 
         if filepath == False:
-                return BadRequest('Wrong type')
+                return Response(
+                        response=json.dumps({'Error':'Wrong type'}),
+                        mimetype='application/json',
+                        status=400
+                )
         
 
         rule = request.get_data()
@@ -73,7 +101,11 @@ def addrule(ty):
         
                 appendData(filepath,rule)
 
-        return jsonify({'msg':'Data added'})
+        return Response(
+                        response=json.dumps({'run_msg':'Data added'}),
+                        mimetype='application/json',
+                        status=200
+                )
 
 #Server route to delete data
 @server.route('/del/<ty>',methods = ['DELETE'])
@@ -82,12 +114,21 @@ def delrule(ty):
 
         filepath = updateFilePath(dirpath,ty) 
         if filepath == False:
-                return BadRequest('Wrong type')
+                return Response(
+                        response=json.dumps({'Error':'Wrong type'}),
+                        mimetype='application/json',
+                        status=400
+                )
 
         id = request.args['id']
         flag = deleteData(filepath,id)
-        message = 'Data deleted' if flag is True else 'Error in deletion'
-        return jsonify({'msg':message})
+        message = 'Data deleted' if flag is True else id+' doesnot exist'
+        sts = 200 if flag is True else 400
+        return Response(
+                        response=json.dumps({'run_msg':message}),
+                        mimetype='application/json',
+                        status=sts
+                )
 
 #Server route to modify data
 @server.route('/modify/<ty>',methods = ['POST'])
@@ -96,16 +137,26 @@ def modifyrule(ty):
 
         filepath = updateFilePath(dirpath,ty) 
         if filepath == False:
-                return BadRequest('Wrong type')
+                return Response(
+                        response=json.dumps({'Error':'Wrong type'}),
+                        mimetype='application/json',
+                        status=400
+                )
 
         flag =False
         rule = request.get_data()
         if rule!=None:
                 rule=eval(rule)
                 flag = updateRule(filepath,rule)
+        
         msg = 'Data updated' if flag else 'Data with specified id not found'
-    return jsonify({'msg':msg})
+        sts = 200 if flag is True else 400
 
+        return Response(
+                        response=json.dumps({'run_msg':msg}),
+                        mimetype='application/json',
+                        status=sts
+                )
 #Server route to get data
 @server.route('/get/<ty>',methods = ['GET'])
 def getrule(ty):
@@ -113,7 +164,11 @@ def getrule(ty):
 
         filepath = updateFilePath(dirpath,ty) 
         if filepath == False:
-                return BadRequest('Wrong type')
+                return Response(
+                        response=json.dumps({'Error':'Wrong type'}),
+                        mimetype='application/json',
+                        status=400
+                )
 
         id = request.args['id']
         if id == 'all':
@@ -122,7 +177,13 @@ def getrule(ty):
             data = getSpecificRule(filepath,id)
 
         message = 'Data found' if data != False else 'data not found'
-        return jsonify({'msg':message,'data':data})
+        sts = 200 if data!=False  else 400
+        return Response(
+                        response=json.dumps({'run_msg':message,'data':data}),
+                        mimetype='application/json',
+                        status=sts
+                )
+        
 
 
 @server.errorhandler(500)
@@ -130,7 +191,9 @@ def internal_error(error):
     print(error)
     return Response(status = 500)
 
-
+@server.errorhandler(405)
+def bad_method(error):
+        return Response(status=405)
 
 if __name__ == "__main__":
     server.run(port = 5000,debug=True)
