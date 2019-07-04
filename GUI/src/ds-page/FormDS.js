1420f6ade150c593b2c4a9ad9cf5584f32a28763
Vij <Amit.Vij@viasat.com>
2019-07-04 17:11:14 +0530

    created data source page

---
+++ GUI/src/ds-page/FormDS.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export class FormDS extends Component {
+    
+    state={
+        
+    }
+    
+    
+    render() {
+        return (
+            <div>
+                Form
+            </div>
+        )
+    }
+}
+
+export default FormDS
