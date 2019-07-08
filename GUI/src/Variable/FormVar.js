2c7382f09e30a8343ede54748206c07718fd7264
Vij <Amit.Vij@viasat.com>
2019-07-08 21:42:02 +0530

    Added variables side bar

---
+++ GUI/src/Variable/FormVar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export class FormVar extends Component {
+    
+    state = {
+        
+    }
+    
+    
+    render() {
+        return (
+            <div>
+                Form
+            </div>
+        )
+    }
+}
+
+export default FormVar
