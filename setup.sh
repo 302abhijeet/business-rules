cd369843f38680598bb6c27a80f4be504ee92ded
Singh <Abhijeet.Singh@viasat.com>
2019-07-22 17:05:17 +0530

    update setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 #setup for pyhton and GUI
 cd GUI
-sudo apt-get install nodejs
+sudo apt-get install nodejs-legacy
 npm install
 cd ..
 cd web-services
 sudo apt-get install python3
+sudo apt-get install pip
 pip install flask
 pip install pymongo
 pip install flask_cors
